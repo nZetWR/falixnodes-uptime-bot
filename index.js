75659b2c52b115623c6de72ecb4f305d85b8ba25
nZetWR <trt1234564@gmail.com>
2026-06-07 01:28:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ function createBot() {
         port: 58392,
         username: 'FlixUpTimeBot',
         version: '1.21.1',
-        checkTimeoutInterval: 60000,
+        checkTimeoutInterval: 120000,
         hideErrors: true
     });
 
     bot.on('spawn', () => {
-        console.log('Бот успешно зашел!');
-        // Прыгаем реже (раз в 15 секунд), чтобы сервер не считал это спамом пакетов
+        console.log('OK');
         setInterval(() => {
             if (bot && bot.entity) {
                 bot.setControlState('jump', true);
@@ -20,15 +19,14 @@ function createBot() {
                     if (bot && bot.entity) bot.setControlState('jump', false);
                 }, 400);
             }
-        }, 15000);
+        }, 20000);
     });
 
-    bot.on('kick', (reason) => console.log(`Кикнули: ${reason}`));
-    bot.on('error', (err) => console.log(`Ошибка: ${err.message}`));
+    bot.on('kick', (reason) => console.log(reason));
+    bot.on('error', (err) => console.log(err.message));
     
     bot.on('end', () => {
-        // Ждем целых 30 секунд перед перезаходом, чтобы сервер успел "забыть" старое соединение
-        setTimeout(createBot, 30000);
+        setTimeout(createBot, 180000);
     });
 }
 
