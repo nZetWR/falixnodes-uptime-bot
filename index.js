55dadf8a9fc258c2973fade3db6132a041c064ac
nZetWR <trt1234564@gmail.com>
2026-06-07 01:37:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,32 @@ function createBot() {
         hideErrors: true
     });
 
+    function doRandomAction() {
+        if (bot && bot.entity) {
+            const actionType = Math.random();
+            if (actionType < 0.5) {
+                bot.swingArm('right');
+            } else {
+                bot.setControlState('sneak', true);
+                setTimeout(() => {
+                    if (bot && bot.entity) bot.setControlState('sneak', false);
+                }, 300);
+            }
+        }
+        const randomTime = Math.floor(Math.random() * (25000 - 8000 + 1)) + 8000;
+        setTimeout(doRandomAction, randomTime);
+    }
+
     bot.on('spawn', () => {
         console.log('OK');
-        setInterval(() => {
-            if (bot && bot.entity) {
-                bot.setControlState('jump', true);
-                setTimeout(() => {
-                    if (bot && bot.entity) bot.setControlState('jump', false);
-                }, 400);
-            }
-        }, 20000);
+        setTimeout(doRandomAction, 5000);
     });
 
     bot.on('kick', (reason) => console.log(reason));
     bot.on('error', (err) => console.log(err.message));
     
     bot.on('end', () => {
-        setTimeout(createBot, 180000);
+        setTimeout(createBot, 45000);
     });
 }
 
