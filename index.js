a9320b8d605f2ccb8b3809e6fe03653a9ef8cff6
nZetWR <trt1234564@gmail.com>
2026-06-06 21:57:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ function createBot() {
         host: 'breakpointsmp.falix.gg',
         port: 58392,
         username: 'FlixUpTimeBot',
-        version: '1.21.11',
+        version: '1.21.1'
     });
 
-    bot.on('kick', (reason) => console.log(Кикнули: ${reason}));
-    bot.on('error', (err) => console.log(Ошибка: ${err}));
+    bot.on('kick', (reason) => console.log(`Кикнули: ${reason}`));
+    bot.on('error', (err) => console.log(`Ошибка: ${err}`));
     
-    // Перезапуск бота при отключении
     bot.on('end', () => {
         console.log('Бот отключился. Перезапуск через 10 секунд...');
         setTimeout(createBot, 10000);
